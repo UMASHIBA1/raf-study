53bbfafbc31a8e9bb874185b96f419db92d417e4
UMASHIBA <umashiba1@gmail.com>
2021-04-21 14:21:03 +0900

    set: setting styled-components

---
+++ src/examples/Example1/Example1.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, Story } from "@storybook/react";
+import React, { ComponentProps } from "react";
+import Example1 from "./Example1";
+
+export default {
+    title: "Example1",
+    compoennt: Example1
+} as Meta;
+
+const Template: Story<ComponentProps<typeof Example1>> = (args) => <Example1 {...args} />;
+
+export const Example1_1 = Template.bind({});
---
+++ src/examples/Example1/Example1.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    width: 360px;
+    height: 360px;
+    background-color: blue;
+`
+
+
+const Example1: React.FC = () => {
+    return(
+        <Wrapper />
+    );
+}
+
+export default Example1;
